7128f20bc757b51312f6d6ed0f79b8c271a63274
hanichris <hanichris71@gmail.com>
2022-07-01 12:22:03 -0700

    Command line arguments in Python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    
+    argc = len(sys.argv)
+    if argc <= 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print(f"{argc - 1} argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{argc - 1} arguments:")
+        for index, argument in enumerate(sys.argv[1:], 1):
+            print(f"{index}: {argument}")
+
