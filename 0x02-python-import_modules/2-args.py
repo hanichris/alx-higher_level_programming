d3ad4e9948113ec9a64880ef929de6237ddd2773
hanichris <hanichris71@gmail.com>
2022-07-01 12:23:43 -0700

    Fix pep8 coding style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    
+
     argc = len(sys.argv)
     if argc <= 1:
         print("0 arguments.")
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         print(f"{argc - 1} arguments:")
         for index, argument in enumerate(sys.argv[1:], 1):
             print(f"{index}: {argument}")
-
