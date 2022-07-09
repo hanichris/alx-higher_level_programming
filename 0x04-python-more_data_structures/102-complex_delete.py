b3f64ec4b42afe70f867a4fcd4c23dbc44dcb054
hanichris <hanichris71@gmail.com>
2022-07-08 18:23:08 -0700

    delete elements from a dictionary with the aid of .copy()

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def complex_delete(a_dictionary, value):
+    for key, val in a_dictionary.copy().items():
+        if val == value:
+            del a_dictionary[key]
+    return a_dictionary
