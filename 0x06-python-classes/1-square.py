03c6d09b01188536e9affa7282bd4af93be705ea
hanichris <hanichris71@gmail.com>
2022-07-19 18:32:34 -0700

    Initialize the Square class with a private attribute.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Represents a Square."""
+
+    def __init__(self, size):
+        """Initialize a square instance.
+
+        Args:
+            size (int): private attribute.
+        """
+        self.__size = size
