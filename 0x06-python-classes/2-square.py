ea7801e912ad286f74856fe347dbba4175178305
hanichris <hanichris71@gmail.com>
2022-07-19 18:42:14 -0700

    Define a class Square with a private instance attribute.
    The private instance varible must be of type int else an exception is
    raised. Also, the attribute should be >= 0 or an exception is raised.

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Module that holds the definition of a Square class."""
+
+class Square:
+    """Models a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square object.
+
+        Args:
+            size (int): The size of the square object.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
