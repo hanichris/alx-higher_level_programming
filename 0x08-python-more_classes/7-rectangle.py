0907acb295fe7a0b35baff186814ae300ab72589
hanichris <hanichris71@gmail.com>
2022-07-28 09:12:38 -0700

    Added a class attribute print_symbol that defines the character to use when printing out the rectangular instance.
    The class attribute has been accessed through the self argument which
    defines the namespace to check through first.

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+"""Module that holds the definition of a Rectangle class."""
+
+
+class Rectangle:
+    """Models a rectangle.
+
+    Attributes:
+        number_of_instances (int): keeps track of the instances created.
+        print_symbol (Any): used as the symbol for string representation.
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangular object.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Get/set the private attribute width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the private attribute height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Compute the area of the rectangle using the A=LW formula."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Compute the perimeter of the rectangle using P=2(L+W)."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Return printable representation of a rectangular object.
+
+        Uses the character referenced by the class attribute
+        print_symbol to represent the rectangle.
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        for row in range(self.height):
+            for col in range(self.width):
+                print(self.print_symbol, end="")
+            if row != self.height - 1:
+                print()
+        return ""
+
+    def __repr__(self):
+        """Return the string representation of a rectangular object.
+
+        This allows for the recreation of a new instance using the
+        eval() function.
+        """
+        rect = f"Rectangle({str(self.width)}, {str(self.height)})"
+        return rect
+
+    def __del__(self):
+        """Print a message during an instance's deletion."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
