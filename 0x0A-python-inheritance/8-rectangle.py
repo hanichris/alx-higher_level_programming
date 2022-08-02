2a1d5323232e3c52df47fa220eb9693a1fbb8b1a
hanichris <hanichris71@gmail.com>
2022-08-02 12:16:42 -0700

    adherance to PEP8 guidelines

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,6 +25,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """Defines a rectangular object by extending BaseGeometry."""
 
