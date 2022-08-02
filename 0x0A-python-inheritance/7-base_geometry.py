214073a570269b57aa0feac2fa1f7cfddc36b594
hanichris <hanichris71@gmail.com>
2022-08-02 11:22:48 -0700

    Added a public instance method to validate `value`.
    The method checks whether `value` is an integer and whether its
    value is greater than 0

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Empty class."""
+
+    def area(self):
+        """Unimplemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value.
+
+        Args:
+            name (str)
+            value (int)
+
+        Raises:
+            TypeError if value is not an integer.
+            ValueError if value is <= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
