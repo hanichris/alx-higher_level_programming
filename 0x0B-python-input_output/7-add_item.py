5a385ff3e5e9bc23894d267288b880349102014c
hanichris <hanichris71@gmail.com>
2022-08-08 16:20:59 -0700

    Script that adds all arguments to a Python list and saves them to a file.

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script to add all command line arguments to a list and
+save the list object to a file.
+"""
+import json
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json_file('add_item.json')
+    except:
+        save_to_json_file([], 'add_item.json')
+    else:
+        for element in sys.argv:
+            if element != './7-add_item.py':
+                my_list.append(element)
+        save_to_json_file(my_list, 'add_item.json')
