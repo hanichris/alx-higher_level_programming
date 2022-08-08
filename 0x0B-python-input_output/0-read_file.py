059b91acb119807c886e770f796a8ba8f8cb6f66
hanichris <hanichris71@gmail.com>
2022-08-08 10:46:35 -0700

    remove trailing newline of print() function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
         filename (str): name of the text file.
     """
     with open(filename, encoding='utf-8') as a_file:
-        print(a_file.read())
+        print(a_file.read(), end="")
