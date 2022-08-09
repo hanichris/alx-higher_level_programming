a66eae96a06b704981e28cb9eeade333d38e63ec
hanichris <hanichris71@gmail.com>
2022-08-09 12:42:13 -0700

    Implement a public method reload_from_json(self, json) that replaces all the attributes of the Student instance with the elements in json.

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module with class Student definition."""
+
+
+class Student:
+    """Models a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student object.
+
+        Args:
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve the dictionary representation of a Student instance.
+
+        Args:
+            attrs (list/None): list of attributes to retrieve. If None,
+            retrieve all the attributes.
+        """
+        if type(attrs) is list:
+            res = dict(filter(lambda x: x[0] in attrs,
+                              self.__dict__.items()))
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces the attributes of self with elements in json.
+
+        Args:
+            json (dict): contains elements to replace self.__dict__ with.
+        """
+        return self.__dict__.update(json)
