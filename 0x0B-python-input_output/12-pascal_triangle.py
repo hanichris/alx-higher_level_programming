2061a82ac03e0c9bada5be756747407a4bc81cb8
hanichris <hanichris71@gmail.com>
2022-08-10 18:21:27 -0700

    Function to return a list of lists that represents Pascal's triangle of n
    The algorithm Time complexity is O(n^2) while the space complexity is O(n^2)

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""Module defining pascal_triangle() function."""
+
+
+def pascal_triangle(n):
+    """Return a list of lists representing Pascal's triangle of n.
+
+    Args:
+        n (int): size of the triangle.
+
+    Return:
+        list(list): Representation of Pascal's triangle.
+    """
+    res = []
+    if n <= 0:
+        return res
+    for row in range(1, n+1):
+        coeff = 1
+        temp = []
+        for k in range(1, row + 1):
+            temp.append(coeff)
+            coeff = int(coeff * (row - k) / k)
+        res.append(temp)
+    return res
