e58266dc699bc9646bc3ee98c0531f1fc0ff59d8
hanichris <hanichris71@gmail.com>
2022-09-08 08:35:46 -0700

    List all the records of the second table in descending order of the scores.

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- List all the records in the table `second_table`.
+-- The records are displayed in the order of score and name.
+-- The records are ordered by the score.
+SELECT score, name FROM second_table ORDER BY score DESC;
