b497b42f1a6d7e07f66cda82bdfb6c77c3711781
hanichris <hanichris71@gmail.com>
2022-09-13 10:03:50 -0700

    SQL script to list the shows which don't have a genre associated with them.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- List all the shows that don't have a genre associated with them
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
