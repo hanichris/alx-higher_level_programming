25c63ab9acf781c75c1203e5e060b57d79558ce5
hanichris <hanichris71@gmail.com>
2022-10-03 18:03:22 -0700

    Recursive implementation of the factorial algorithm

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,6 +7,6 @@ if (isNaN(number)) console.log(1);
 else console.log(factorial(number));
 
 function factorial (n) {
-	if (n === 0 || n === 1) return 1;
-	return n * factorial(n-1);
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 }
