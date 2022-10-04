38bddf66844fd837155792dcac78e3809964db8f
hanichris <hanichris71@gmail.com>
2022-10-03 18:01:15 -0700

    Recursive implementation of a factorial program

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+'use strict';
+
+const number = +process.argv[2];
+
+if (isNaN(number)) console.log(1);
+else console.log(factorial(number));
+
+function factorial (n) {
+	if (n === 0 || n === 1) return 1;
+	return n * factorial(n-1);
+}
