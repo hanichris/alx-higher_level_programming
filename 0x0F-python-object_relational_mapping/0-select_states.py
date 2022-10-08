ec116634d16956ea90c9eb28d29cc4d07eb3f064
hanichris <hanichris71@gmail.com>
2022-10-08 08:56:41 -0700

    Python script that lists all the `states` for the database `hbtn_0e_0_usa`.
    The username, password and database name are passed as command-line arguments to
    the script. The code is guarded against running when imported as a module.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Python script that uses the module `MySQLdb`.
+
+The script utilises the module to connect to a database and
+return a list of the states from it.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=passwd, db=database)
+    cur = db.cursor()
+    query = "SELECT id, name FROM states ORDER BY id"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close
