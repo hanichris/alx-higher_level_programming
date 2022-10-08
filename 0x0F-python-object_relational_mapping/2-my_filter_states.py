e23e67045664cff97a8fade439988a0b6d787566
hanichris <hanichris71@gmail.com>
2022-10-08 09:30:02 -0700

    Python script to select a particular row from a database table that matches the string passed in as a command-line argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Python script that uses the module `MySQLdb`.
+
+The script utilises the module to connect to a database and
+return a list of the states from it.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=passwd, db=database)
+    cur = db.cursor()
+    query = """SELECT id, name FROM states
+               WHERE name = '{}' ORDER BY id""".format(state)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close
