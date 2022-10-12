5f1559fbe496c4c280a08ea547de9ea9c91b4ea5
hanichris <hanichris71@gmail.com>
2022-10-12 08:52:24 -0700

    Python script that uses the module `SQLAlchemy` to connect to a database and selecting all the `State` objects in an ascending order.

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Using python module `SQLAlchemy` to list all the State objects."""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import select, create_engine
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    url = f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
+    engine = create_engine(url, pool_pre_ping=True)
+
+    stmt = select(State).order_by(State.id.asc())
+    with Session(engine) as session:
+        for state in session.execute(stmt).scalars():
+            print(f'{state.id}: {state.name}')
