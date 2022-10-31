26ad2e843e8c6c98bccf8c705c99b25f484963a6
root <hanichris71@gmail.com>
2022-10-31 13:11:24 -0700

    Use import guard

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,9 +5,10 @@ import urllib.request
 import urllib.parse
 
 
-value = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(value)
-data = data.encode('utf-8')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == '__main__':
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
