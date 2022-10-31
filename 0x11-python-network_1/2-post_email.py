589fa6b2d36717caa4c82d23f00d15bd323887aa
root <hanichris71@gmail.com>
2022-10-31 13:02:55 -0700

    Python scripts to send a POST request and GET request to get a particular header.

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script to send a `POST` request to the passed url."""
+import sys
+import urllib.request
+import urllib.parse
+
+
+value = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(value)
+data = data.encode('utf-8')
+req = urllib.request.Request(sys.argv[1], data)
+with urllib.request.urlopen(req) as response:
+    print(response.read().decode('utf-8'))
