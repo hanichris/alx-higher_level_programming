3c81d36cae5de8e04da1cf1c2c754a00b42e9c24
root <hanichris71@gmail.com>
2022-10-31 12:46:26 -0700

    Python script to make a get request to a url and get the headers. Pick out the header of interest.

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script to send a request to the provided url.
+
+On receiving the response, pick out the header with the name
+`X-Request-Id`.
+"""
+import sys
+import urllib.request
+
+
+req = urllib.request.Request(sys.argv[1])
+with urllib.request.urlopen(req) as response:
+    headers = response.info()
+    print(headers['X-Request-Id'])
