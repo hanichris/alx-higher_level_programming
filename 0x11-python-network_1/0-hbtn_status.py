df27937bef0bed55805fa5b947fdfa213d028143
root <hanichris71@gmail.com>
2022-10-31 12:35:37 -0700

    use `\t` to format the output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
-    _type = f"- type: {type(the_page)}\n"
-    _cxt = f"- content: {the_page}\n"
-    _utf = f"- utf8 content: {the_page.decode('utf-8')}"
+    _type = f"\t- type: {type(the_page)}\n"
+    _cxt = f"\t- content: {the_page}\n"
+    _utf = f"\t- utf8 content: {the_page.decode('utf-8')}"
     print(f"Body response:\n{_type}{_cxt}{_utf}")
