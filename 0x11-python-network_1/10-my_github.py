c20fd791bb7ef7dd6bd716c3e8e003b7fd124b15
root <hanichris71@gmail.com>
2022-11-01 14:37:20 -0700

    used a different access point

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,11 +2,12 @@
 """Display id of GitHub credentials using the GitHub API."""
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
-    url = f'https://api.github.com/users/{user}'
-    res = requests.get(url, auth=(user, passwd))
+    basic = HTTPBasicAuth(user, passwd)
+    res = requests.get('https://api.github.com/user', auth=basic)
     print(res.json().get('id'))
