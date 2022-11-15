2d823ec30c49cc5d61f2835a3420719945dc3141
root <hanichris71@gmail.com>
2022-11-15 22:56:30 +0000

    Print the number of movies where a particular character is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,15 @@
 const request = require('request');
 
 const url = process.argv[2];
-const c_id = 18;
+const cId = 18;
 
 request(url, (err, resp, body) => {
   if (err) console.log(err);
   else {
     const films = JSON.parse(body);
     const results = films.results.filter(
-        film => film.characters.find(
-            id => id.match(c_id)));
+      film => film.characters.find(
+        id => id.match(cId)));
     console.log(results.length);
   }
 });
