42a39052b1b698c2a6d2e7ab70f1b31f5a25d9f0
root <hanichris71@gmail.com>
2022-11-15 22:08:25 +0000

    Removed extra spacing in the output.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, resp, body) => {
   if (err) console.log(err);
-  else console.log('code: ', resp.statusCode);
+  else console.log('code:', resp.statusCode);
 });
