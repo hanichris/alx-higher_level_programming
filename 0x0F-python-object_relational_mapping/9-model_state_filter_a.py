9a90bcce4f74f4d640447fee32fb7e7b55528099
root <hanichris71@gmail.com>
2023-01-20 09:10:21 -0800

    change

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Using python module `SQLAlchemy` to list all the State objects."""
+"""
+Lists all State objects that contain the letter a from the database
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import Session
-from sqlalchemy import select, create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
 
-    url = f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
-    engine = create_engine(url, pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    stmt = select(State).filter(State.name.ilike('%a%')).\
-        order_by(State.id)
-    with Session(engine) as session:
-        for state in session.scalars(stmt):
-            print(f'{state.id}: {state.name}')
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
